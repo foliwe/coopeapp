9d79e5c47a0f24a34726cfb9d121cdb7fa23f3c5
foliwe <foliwe@gmail.com>
2021-02-13 12:47:51 +0000

    compile false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  #config.assets.compile = false
+   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
@@ -53,7 +53,7 @@ Rails.application.configure do
   # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
   # for assets compile
-  config.assets.compile = true
+  #config.assets.compile = true
   config.assets.digest = true
 
   # Prepend all log lines with the following tags.
