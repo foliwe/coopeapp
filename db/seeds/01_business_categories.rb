b2446c2b7b399c7dce3c61975e677072aaf4cf56
foliwe <foliwe@gmail.com>
2021-03-02 00:50:54 +0000

    merging

--- db/seeds/01_business_categories.rb
+++ db/seeds/01_business_categories.rb
@@ -1,5 +1,10 @@
 require 'faker'
 
+Continent.create!(name: "Africa")
+Continent.create!(name: "Asia")
+Continent.create!(name: "Europe")
+Continent.create!(name: "North America")
+Continent.create!(name: "South America")
 
 20.times do 
     Category.create!(name: Faker::Company.industry)
